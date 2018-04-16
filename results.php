974a32570b79e36244d7f036def5838b6f1902f5
sakura hitachi <saikal0515@gmail.com>
2018-04-16 10:06:02 +0600

    file changes

--- results.php
+++ results.php
@@ -24,8 +24,8 @@ if (!$video->status['success']) {
     <title>Youtube DL PHP</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-    <link rel="stylesheet" href="../styles/reset.css">
-    <link rel="stylesheet" href="../styles/style.css">
+    <link rel="stylesheet" href="styles/reset.css">
+    <link rel="stylesheet" href="styles/style.css">
   
 </head>
 <body>
